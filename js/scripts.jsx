730b3e51bf895b984ededfb8ebc44310491e8a53
dahuatool <47340323+dahuatool@users.noreply.github.com>
2019-06-12 10:48:59 -0500

    Add files via upload
      Dahua tool v.1.4
      - Multiple fields capability for model/serial/firmware added
      - Fixed unable to add text at begining of fields

--- js/scripts.jsx
+++ js/scripts.jsx
@@ -1,5 +1,5 @@
 // Global text from main textarea
-var mainText = document.getElementById("mainText");
+let mainText = document.getElementById("mainText");
 
 // Global string defaultHash, stores hash in its original form
 // inputID:[templateText,isMandatory,value]
@@ -23,12 +23,16 @@ const defaultHash = {
     "inputMissing": ["\nMissing Steps", false, ""]
 };
 
+// Global letiable mainFields, tracks how many additional models/serials/firmware fields have been added
+// Global const, maximum number of fields to prevent user to get confused
+let mainFields = 1;
+const maxFields = 4;
 
 // TODO
 // Add event listener to elements in the dropdown menu
 // Couldn't make it work even with anonymous functions, switched to old school
 
-// var names = document.querySelectorAll(".dropdown-item");
+// let names = document.querySelectorAll(".dropdown-item");
 
 // for (let i in names) {
 //   document.addEventListener('click', function() {
@@ -37,16 +41,16 @@ const defaultHash = {
 // }
 
 // Will try this format:
-// var x = document.getElementById("demo");
+// let x = document.getElementById("demo");
 // x.onclick = function () {
 //   document.body.innerHTML = Date();
 
 // Global string formHash, copies defaultHash
-var formHash = JSON.parse(JSON.stringify(defaultHash));
+let formHash = JSON.parse(JSON.stringify(defaultHash));
 
 // Global current date in the required format
-var d = new Date();
-var currentDate = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
+let d = new Date();
+let currentDate = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
 
 // checkVersion() Shows the latest features of the version on the first load of the app.
 // Created after noticing people were unaware of the features of the app
@@ -54,12 +58,12 @@ function checkVersion() {
     if (localStorage.checkVersion != "true") {
         $(document).ready(function () {
             $("#checkVersion").modal();
-        });        
+        });
     }
     localStorage.setItem("checkVersion", "true");
 }
 
-// setName() Stores the name of the user on the local storage and updates the dropdownmenu
+// setName(element) Stores the name of the user on the local storage and updates the dropdownmenu
 function setName(element) {
     localStorage.setItem("wizardName", element.textContent);
 
@@ -69,15 +73,53 @@ function setName(element) {
 
 // updateTextarea(element) Textarea gets updated automatically
 function updateTextarea(element) {
-    var str = document.getElementById("dropdownWizard").innerHTML + " - " + currentDate + "\n\n";
-    // update hash with the new info, reset if needed
-    if (element == "reset")
-        formHash = JSON.parse(JSON.stringify(defaultHash));
-    else
-        formHash[element.id][2] = element.value;
+    let str = document.getElementById("dropdownWizard").innerHTML + " - " + currentDate + "\n\n";
+    let model = document.getElementById("inputModel");
+    let serial = document.getElementById("inputSerial");
+    let firmware = document.getElementById("inputFirmware");
 
-    // create and output string from hash
-    for (var i in formHash) {
+    // selects all model fields by filtering for the id name
+    let allmodels = Array.from(document.querySelectorAll('[id^=inputModel]'));
+    let allserials = Array.from(document.querySelectorAll('[id^=inputSerial]'));
+    let allfirmwares = Array.from(document.querySelectorAll('[id^=inputFirmware]'));
+
+    // update hash with the new info, reset if needed
+    // and create strings needed for multiple fields under the same category
+    if (element == "reset") {
+        formHash = JSON.parse(JSON.stringify(defaultHash));
+    } else if (allmodels.includes(element)) {
+        let string = "";
+        for (let i = 0; i < allmodels.length; i++) {
+            string += allmodels[i].value + " ";
+            if (allmodels[i + 1] && allmodels[i+1].value != "") {
+                string += "// ";
+            }
+        }
+        formHash[model.id][2] = string;
+    } else if (allserials.includes(element)) {
+        let string = "";
+        for (let i = 0; i < allserials.length; i++) {
+            string += allserials[i].value + " ";
+            if (allserials[i + 1] && allserials[i+1].value != "") {
+                string += "// ";
+            }
+        }
+        formHash[serial.id][2] = string;
+    } else if (allfirmwares.includes(element)) {
+        let string = "";
+        for (let i = 0; i < allfirmwares.length; i++) {
+            string += allfirmwares[i].value + " ";
+            if (allfirmwares[i + 1] && allfirmwares[i+1].value != "") {
+                string += "// ";
+            }
+        }
+        formHash[firmware.id][2] = string;
+    } else {
+        formHash[element.id][2] = element.value;
+    }
+
+    // create and output final string from hash
+    for (let i in formHash) {
         // if its empty or na and optional
         if ((formHash[i][2] == "" || formHash[i][2] == "na") && !formHash[i][1]) {
             str += "";
@@ -105,19 +147,92 @@ function updateTextarea(element) {
     mainText.value = str;
 }
 
+// addFields() adds additional model/serial/firmware fields on the main app
+function addFields() {
+    if (mainFields < maxFields) {
+        mainFields++;
+
+        let button = document.getElementById("addNewFields");
+
+        let model = document.createElement("input");
+        let serial = document.createElement("input");
+        let firmware = document.createElement("input");
+
+        let modelParent = document.getElementById("inputModel").parentNode;
+        let currentSerialNode = document.getElementById("inputSerial").parentNode.lastElementChild;
+        let currentFirmwareNode = document.getElementById("inputFirmware").parentNode.lastElementChild;
+
+        model.setAttribute("list", "deviceList");
+        model.classList.add("form-control");
+        model.classList.add("form-control-sm");
+        model.classList.add("uppercase");
+        model.setAttribute("id", `inputModel${mainFields}`);
+        model.placeholder = "Model Number";
+        model.setAttribute("onInput", "updateTextarea(this)");
+        
+        serial.classList.add("form-control");
+        serial.classList.add("form-control-sm");
+        serial.classList.add("uppercase");
+        serial.setAttribute("id", `inputSerial${mainFields}`);
+        serial.placeholder = "Serial Number";
+        serial.setAttribute("onInput", "updateTextarea(this)");        
+
+        firmware.classList.add("form-control");
+        firmware.classList.add("form-control-sm");
+        firmware.setAttribute("id", `inputFirmware${mainFields}`);
+        firmware.placeholder = "Firmware";
+        firmware.setAttribute("onInput", "updateTextarea(this)");        
+
+        modelParent.insertBefore(model, button);
+        currentSerialNode.parentNode.insertBefore(serial, currentSerialNode.nextElementSibling);
+        currentFirmwareNode.parentNode.insertBefore(firmware, currentFirmwareNode.nextElementSibling);
+    }
+}
+
+// loadStorage() Loads name of the user
 function loadStorage() {
     if (localStorage.wizardName) {
         document.getElementById("dropdownWizard").innerHTML = localStorage.wizardName;
     }
 }
 
+// formReset(), resets form to default state
 function formReset() {
     document.getElementById("dataForm").reset();
     let sources = document.getElementById("inputSources");
     sources.className = "form-control form-control-sm";
+
+    // remove any extrafields created
+    let allmodels = Array.from(document.querySelectorAll('[id^=inputModel]'));
+    let allserials = Array.from(document.querySelectorAll('[id^=inputSerial]'));
+    let allfirmwares = Array.from(document.querySelectorAll('[id^=inputFirmware]'));
+
+    for (let i = 1; i < allmodels.length; i++) {
+        allmodels[i].parentNode.removeChild(allmodels[i]);
+        allserials[i].parentNode.removeChild(allserials[i]);
+        allfirmwares[i].parentNode.removeChild(allfirmwares[i]);
+    }
+
+    mainFields = 1;
     updateTextarea("reset");
 }
 
+// Removes the last fields on the linked model/serial/firmware fields
+function removeLastField() {
+    let allmodels = Array.from(document.querySelectorAll('[id^=inputModel]'));
+    let allserials = Array.from(document.querySelectorAll('[id^=inputSerial]'));
+    let allfirmwares = Array.from(document.querySelectorAll('[id^=inputFirmware]'));
+
+    if (mainFields > 1) {
+        allmodels[0].parentNode.removeChild(allmodels[allmodels.length - 1]);
+        allserials[0].parentNode.removeChild(allserials[allserials.length - 1]);
+        allfirmwares[0].parentNode.removeChild(allfirmwares[allfirmwares.length - 1]);
+
+        mainFields--;
+    }
+}
+
+// copyTextArea() copies the text on the main textarea to the clipboard. Also highlights missing fields
 function copyTextArea() {
     let sources = document.getElementById("inputSources");
     if (!sources.value) {
@@ -129,19 +244,15 @@ function copyTextArea() {
     document.execCommand("copy");
 }
 
-function switchCase(obj) {
-    obj.value = obj.value.toUpperCase()
-}
-
 // Templates. TODO: 1 function for all templates
 
 // Password Reset
 function passwordReset() {
-    var templateText = JSON.parse(JSON.stringify(formHash));
+    let templateText = JSON.parse(JSON.stringify(formHash));
     templateText.inputProblem[0, 2] = "Password Reset";
     templateText.inputTx[0, 2] = "Date on the screen is <INSERT DATE HERE>.\nSent email with the passwords and asked to follow the instructions included.";
 
-    for (var k in templateText) {
+    for (let k in templateText) {
         document.getElementById(k).value = templateText[k][2];
     }
 
@@ -154,7 +265,7 @@ function passwordReset() {
 
 // Remote connection setup, thank you Miguel!
 function P2PSetup(interface) {
-    var templateText = JSON.parse(JSON.stringify(formHash));
+    let templateText = JSON.parse(JSON.stringify(formHash));
     templateText.inputProblem[0, 2] = "P2P connection setup";
     if (interface == "Blue") {
         templateText.inputTx[0, 2] = "On the Unit: Main Menu > Network (Setting) > P2P and enabled P2P. Click OK.\r" +
@@ -183,7 +294,7 @@ function P2PSetup(interface) {
         templateText.inputSources[0, 2] = "https://dahuawiki.com/New_GUI/Instructions/Remote_Access_via_P2P";
     }
 
-    for (var k in templateText) {
+    for (let k in templateText) {
         document.getElementById(k).value = templateText[k][2];
     }
 
@@ -194,7 +305,7 @@ function P2PSetup(interface) {
 
 // Motion Setup
 function motionRecording(interface) {
-    var templateText = JSON.parse(JSON.stringify(formHash));
+    let templateText = JSON.parse(JSON.stringify(formHash));
     templateText.inputProblem[0, 2] = "Motion recording setup";
     if (interface == "Blue") {
         templateText.inputTx[0, 2] = "Main Menu > Event under Settings > Video Detect > Motion Detect > Enable.\r" +
@@ -210,7 +321,7 @@ function motionRecording(interface) {
         templateText.inputSources[0, 2] = "https://dahuawiki.com/New_GUI/Instructions/Motion_Record";
     }
 
-    for (var k in templateText) {
+    for (let k in templateText) {
         document.getElementById(k).value = templateText[k][2];
     }
 
@@ -220,7 +331,7 @@ function motionRecording(interface) {
 
 // Email notifications setup
 function emailNotifications(interface) {
-    var templateText = JSON.parse(JSON.stringify(formHash));
+    let templateText = JSON.parse(JSON.stringify(formHash));
     templateText.inputProblem[0, 2] = "Email Notifications Setup";
     let commonText = "Server smtp.gmail.com, port 465.\r" +
         "Username is the full gmail address, password for gmail account.\r" +
@@ -244,12 +355,12 @@ function emailNotifications(interface) {
         templateText.inputSoftware[0, 2] = "SmartPSS"
     }
 
-    for (var k in templateText) {
+    for (let k in templateText) {
         document.getElementById(k).value = templateText[k][2];
     }
 
     updateTextarea(document.getElementById("inputProblem"));
-    updateTextarea(document.getElementById("inputTx"));    
+    updateTextarea(document.getElementById("inputTx"));
 }
 
 window.onload = function () {
